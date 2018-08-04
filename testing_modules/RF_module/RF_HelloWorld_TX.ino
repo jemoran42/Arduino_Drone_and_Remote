529fe0398c9f23946ccb27526a5bb4cd8fc1ed32
Jacob Moran <jmoran0405@gmail.com>
2018-08-04 15:10:56 -0500

    reordering directories

---
+++ testing_modules/RF_module/RF_HelloWorld_TX.ino
@@ -0,0 +1,24 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8);
+
+const byte rxAddr[6] = "00001";
+
+void setup()
+{
+  radio.begin();
+  radio.setRetries(15, 15);
+  radio.openWritingPipe(rxAddr);
+  
+  radio.stopListening();
+}
+
+void loop()
+{
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  
+  delay(1000);
+}
